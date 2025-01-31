146a1a08d77ffe730ee56d4540c8c8c55faabf72
Argue <Argue@LAPTOP-O82B69D3>
2025-01-31 17:45:50 -0600

    subtraction added

--- src/Day2/DemoDay2.java
+++ src/Day2/DemoDay2.java
@@ -1,10 +1,22 @@
 package Day2;
 
 public class DemoDay2 {
-	public static void main(String[] args) {
-		System.out.println("Semehar");
-		//haha
-		//love java
-		//this is coming from GitHub
-}
+    public static void main(String[] args) {
+        System.out.println("Semehar");
+        // haha
+        // love java
+        // this is coming from GitHub
+
+        int num1 = 20;
+        int num2 = 5;
+
+        System.out.println("Subtraction Result: " + subtract(num1, num2));
+        System.out.println("Division Result: " + divide(num1, num2));
+    }
+
+    // Method to subtract two numbers
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
 }
